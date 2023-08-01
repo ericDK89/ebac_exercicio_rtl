5f2cd40c79b7030c40d62a2a765b62dd64716c9b
ericDK89 <ericthr42@gmail.com>
2023-08-01 15:46:47 -0300

    modulo-33

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,18 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it("should have two comments", () => {
+        render(<PostComment/>);
+        const button = screen.getByTestId("add-btn")
+        const newText = screen.getByTestId("text-value")
+
+        fireEvent.change(newText, {target: {value: "Test"}})        
+        fireEvent.click(button)
+
+        fireEvent.change(newText, {target: {value: "Test"}})
+        fireEvent.click(button)
+
+        expect(screen.getAllByText('Test')).toHaveLength(2);
+    })
 });
